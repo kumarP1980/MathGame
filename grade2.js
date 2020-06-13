7c174191d84828d046c2e0539eb8f4861fc5f88a
kumarP1980 <58350714+kumarP1980@users.noreply.github.com>
2020-06-13 18:59:56 -0400

    Added new files
    Updated pages for grade1 and grade2 or up.

---
+++ grade2.js
@@ -0,0 +1,113 @@
+// Hide values
+$(document).ready(function(){
+  $("#ans").hide();
+  $("#check").hide();
+});
+
+// On Add button click
+  $("#add").click(function(){
+    var randomNumber1 = Math.floor(Math.random() * 31);
+    var randomNumber2 = Math.floor(Math.random() * 31);
+    $("#num1").text(randomNumber1);
+    $("#num2").text(randomNumber2);
+    $("#opt").text("+");
+    $("#eql").text("=");
+    $("h3").show();
+    $("#ans").show();
+    $("#check").show();
+    $("#ans").val("");
+    $("#checkans").hide();
+    $("#check").click(function(){
+      ans = document.getElementById("ans").value;
+      if ((randomNumber1+randomNumber2) == parseInt(ans)){
+        $("#checkans").show();
+        $("#checkans").text("Correct");
+        $("#checkans").css({'color': '#52de97'});
+        } else {
+          $("#checkans").show();
+        $("#checkans").text("Wrong");
+        $("#checkans").css({'color': '#9d2503'});
+      }
+    });
+  });
+
+  // On Substraction button click
+    $("#minus").click(function(){
+      var randomNumber1 = Math.floor(Math.random() * 21);
+      var randomNumber2 = Math.floor(Math.random() * 20);
+      $("#num1").text(randomNumber1);
+      $("#num2").text(randomNumber2);
+      $("#opt").text("-");
+      $("#eql").text("=");
+      $("h3").show();
+      $("#ans").show();
+      $("#check").show();
+      $("#ans").val("");
+      $("#checkans").hide();
+      $("#check").click(function(){
+        ans = document.getElementById("ans").value;
+        if ((randomNumber1-randomNumber2) == parseInt(ans)){
+          $("#checkans").show();
+          $("#checkans").text("Correct");
+          $("#checkans").css({'color': '#52de97'});
+          } else {
+            $("#checkans").show();
+            $("#checkans").text("Wrong");
+            $("#checkans").css({'color': '#9d2503'});
+        }
+      });
+    });
+
+    // On Multiply button click
+      $("#multiply").click(function(){
+        var randomNumber1 = Math.floor(Math.random() * 11);
+        var randomNumber2 = Math.floor(Math.random() * 11);
+        $("#num1").text(randomNumber1);
+        $("#num2").text(randomNumber2);
+        $("#opt").text("x");
+        $("#eql").text("=");
+        $("h3").show();
+        $("#ans").show();
+        $("#check").show();
+        $("#ans").val("");
+        $("#checkans").hide();
+        $("#check").click(function(){
+          ans = document.getElementById("ans").value;
+          if ((randomNumber1*randomNumber2) == parseInt(ans)){
+            $("#checkans").show();
+            $("#checkans").text("Correct");
+            $("#checkans").css({'color': '#52de97'});
+            } else {
+              $("#checkans").show();
+              $("#checkans").text("Wrong");
+              $("#checkans").css({'color': '#9d2503'});
+          }
+        });
+      });
+
+      // On Divide button click
+        $("#divide").click(function(){
+          var randomNumber1 = Math.floor(Math.random() * 61);
+          var randomNumber2 = Math.floor(Math.random() * 7);
+          $("#num1").text(randomNumber1);
+          $("#num2").text(randomNumber2);
+          $("#opt").text("÷");
+          $("#eql").text("=");
+          $("h3").show();
+          $("#ans").show();
+          $("#check").show();
+          $("#ans").val("");
+          $("#checkans").hide();
+          $("#check").click(function(){
+            ans = document.getElementById("ans").value;
+            if ((randomNumber1/randomNumber2).toFixed(2) == parseFloat(ans)){
+              $("#checkans").show();
+              $("#checkans").text("Correct");
+              $("#checkans").css({'color': '#52de97'});
+              } else {
+                $("#checkans").show();
+                $("#checkans").text("Wrong");
+                $("#checkans").css({'color': '#9d2503'});
+            }
+          });
+        });
